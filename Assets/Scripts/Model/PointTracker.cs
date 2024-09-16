301a7ceb6a5618c8df463fc54cd9e3685ca9cef2
WilliamHeidemann <Williamskou2227@gmail.com>
2024-09-16 21:36:51 +0200

    added point tracker classes

---
+++ Assets/Scripts/Model/PointTracker.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UtilityToolkit.Runtime;
+
+namespace Model
+{
+    public class PointTracker
+    {
+        private readonly Observable<int> _points;
+        private readonly TextMeshProUGUI _scoreText;
+        
+        public PointTracker(TextMeshProUGUI scoreText)
+        {
+            _scoreText = scoreText;
+            _points = new Observable<int>();
+            _points.OnValueChanged += UpdateScoreText;
+        }
+
+        private void UpdateScoreText(int points)
+        {
+            _scoreText.text = points.ToString();
+        }
+
+        public void AddPoints(int points)
+        {
+            _points.Value += points;
+        }
+    }
+}
