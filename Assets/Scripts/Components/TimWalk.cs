ab4fec262520710d52d9d1da227d56df5a1853a6
WilliamHeidemann <Williamskou2227@gmail.com>
2024-09-19 12:41:13 +0200

    background and timmy

--- Assets/Scripts/Components/TimWalk.cs
+++ Assets/Scripts/Components/TimWalk.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class TimWalk : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Vector3 startPosition;
-    public Vector3 endPosition;
+    public Transform start;
+    public Transform finish;
+    
     public float speed = 1.0f;
 
     private float elapsedTime = 0.0f;
@@ -13,16 +14,10 @@ public class TimWalk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {  
-
-
-
-        if (transform.position != endPosition)	
+        if (transform.position != finish.position)	
         {
             elapsedTime += Time.deltaTime * speed;
-            transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime);
-
+            transform.position = Vector3.Lerp(start.position, finish.position, elapsedTime);
         }
-
-        
     }
 }
