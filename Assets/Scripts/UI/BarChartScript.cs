b1dad5687217df49b1f2f4206a034cac2ca29948
Kristóf Lénárd <55843722+KristofLenard12@users.noreply.github.com>
2024-09-19 12:15:18 +0200

    Added prefabs, WIP on charts

---
+++ Assets/Scripts/UI/BarChartScript.cs
@@ -0,0 +1,54 @@
+using Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarChartScript : MonoBehaviour
+{
+    public WispBarChart sunChart;
+    public WispBarChart rainChart;
+    public WispBarChart snowChart;
+    public WeatherForecast forecast;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        forecast = new WeatherForecast();
+        GenerateValues();
+    }
+
+    void GenerateValues()
+    {
+        var dict = forecast.GetDictionary();
+        Dictionary<string, float> sunValues = new Dictionary<string, float>();
+        Dictionary<string, float> rainValues = new Dictionary<string, float>();
+        Dictionary<string, float> snowValues = new Dictionary<string, float>();
+        foreach (var item in dict)
+        {
+            print(item.Key);
+            print(item.Value);
+            if (item.Key is Weather.Snowy)
+            {
+                snowValues.Add("SunPoints", item.Value.SunPoints);
+                snowValues.Add("RainPoints", item.Value.RainPoints);
+                snowValues.Add("SnowPoints", item.Value.SnowPoints);
+            }
+            if (item.Key is Weather.Rainy)
+            {
+                rainValues.Add("SunPoints", item.Value.SunPoints);
+                rainValues.Add("RainPoints", item.Value.RainPoints);
+                rainValues.Add("SnowPoints", item.Value.SnowPoints);
+            }
+            if (item.Key is Weather.Sunny)
+            {
+                sunValues.Add("SunPoints", item.Value.SunPoints);
+                sunValues.Add("RainPoints", item.Value.RainPoints);
+                sunValues.Add("SnowPoints", item.Value.SnowPoints);
+            }
+        }
+
+        sunChart.DrawChart(sunValues, 0, 3000, 0, "");
+        rainChart.DrawChart(rainValues, 0, 3000, 0, "");
+        snowChart.DrawChart(snowValues, 0, 3000, 0, "");
+    }
+}
