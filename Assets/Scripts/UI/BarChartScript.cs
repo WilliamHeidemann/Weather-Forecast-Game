28884fdd65e129a0c31162f7f6fc041e90cbf878
Kristóf Lénárd <55843722+KristofLenard12@users.noreply.github.com>
2024-09-19 13:30:31 +0200

    Added grid, updated UI scene

--- Assets/Scripts/UI/BarChartScript.cs
+++ Assets/Scripts/UI/BarChartScript.cs
@@ -1,13 +1,18 @@
 using Model;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using System;
 
 public class BarChartScript : MonoBehaviour
 {
     public WispBarChart sunChart;
     public WispBarChart rainChart;
     public WispBarChart snowChart;
+    public TMP_Text sunChance;
+    public TMP_Text rainChance;
+    public TMP_Text snowChance;
     public WeatherForecast forecast;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +20,7 @@ public class BarChartScript : MonoBehaviour
     {
         forecast = new WeatherForecast();
         GenerateValues();
+        AddGridValues();
     }
 
     void GenerateValues()
@@ -23,6 +29,9 @@ public class BarChartScript : MonoBehaviour
         Dictionary<string, float> sunValues = new Dictionary<string, float>();
         Dictionary<string, float> rainValues = new Dictionary<string, float>();
         Dictionary<string, float> snowValues = new Dictionary<string, float>();
+        print(forecast.ChanceOfSun);
+        print(forecast.ChanceOfRain);
+        print(forecast.ChanceOfSnow);
         foreach (var item in dict)
         {
             print(item.Key);
@@ -47,8 +56,18 @@ public class BarChartScript : MonoBehaviour
             }
         }
 
-        sunChart.DrawChart(sunValues, 0, 3000, 0, "");
-        rainChart.DrawChart(rainValues, 0, 3000, 0, "");
-        snowChart.DrawChart(snowValues, 0, 3000, 0, "");
+        sunChart.DrawChart(sunValues, 0, 0, 0, "Points for sunglasses");
+        rainChart.DrawChart(rainValues, 0, 0, 0, "Points for umbrella");
+        snowChart.DrawChart(snowValues, 0, 0, 0, "Points for skis");
+    }
+
+    void AddGridValues()
+    {
+        int a = (int)(Math.Round(forecast.ChanceOfSun * 100));
+        int b = (int)(Math.Round(forecast.ChanceOfRain * 100));
+        int c = 100-a-b;
+        sunChance.SetText(a.ToString());
+        rainChance.SetText(b.ToString());
+        snowChance.SetText(c.ToString());
     }
 }
