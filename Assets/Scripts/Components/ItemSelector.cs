573b2c20d3818633d8ab038efe875e90498e4374
WilliamHeidemann <Williamskou2227@gmail.com>
2024-09-19 13:01:50 +0200

    gear canvas

--- Assets/Scripts/Components/ItemSelector.cs
+++ Assets/Scripts/Components/ItemSelector.cs
@@ -1,16 +1,22 @@
 using System;
 using Model;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Components
 {
     public class ItemSelector : MonoBehaviour
     {
         private WeatherPrediction _weatherPrediction;
+        [SerializeField] private Color notSelected;
+        [SerializeField] private Color selected;
+        [SerializeField] private Image image1;
+        [SerializeField] private Image image2;
+        [SerializeField] private Image image3;
 
         private void Start()
         {
-            _weatherPrediction = new WeatherPrediction();
+            _weatherPrediction = new WeatherPrediction(notSelected, selected, image1, image2, image3);
         }
 
         private void Update()
--- Assets/Scripts/Model/WeatherPrediction.cs
+++ Assets/Scripts/Model/WeatherPrediction.cs
@@ -1,4 +1,6 @@
 using System;
+using UnityEngine;
+using UnityEngine.UI;
 using UtilityToolkit.Runtime;
 
 namespace Model
@@ -6,9 +8,19 @@ namespace Model
     public class WeatherPrediction
     {
         private readonly Observable<Weather> _prediction;
+        private Color _notSelected;
+        private Color _selected;
+        private Image _image1;
+        private Image _image2;
+        private Image _image3;
 
-        public WeatherPrediction()
+        public WeatherPrediction(Color notSelected, Color selected, Image image1, Image image2, Image image3)
         {
+            _notSelected = notSelected;
+            _selected = selected;
+            _image1 = image1;
+            _image2 = image2;
+            _image3 = image3;
             _prediction = new Observable<Weather>(Weather.Sunny);
             _prediction.OnValueChanged += ChangeUI;
         }
@@ -32,7 +44,26 @@ namespace Model
         
         private void ChangeUI(Weather weatherSelection)
         {
-            throw new NotImplementedException();
+            switch (weatherSelection)
+            {
+                case Weather.Sunny:
+                    _image1.color = _selected;
+                    _image2.color = _notSelected;
+                    _image3.color = _notSelected;
+                    break;
+                case Weather.Rainy:
+                    _image1.color = _notSelected;
+                    _image2.color = _selected;
+                    _image3.color = _notSelected;
+                    break;
+                case Weather.Snowy:
+                    _image1.color = _notSelected;
+                    _image2.color = _notSelected;
+                    _image3.color = _selected;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(weatherSelection), weatherSelection, null);
+            }
         }
     }
 }
